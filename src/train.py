abcc94974a8d15e4983b2c60a8b75f7cfb48dda9
Patrick French <psfrench76@gmail.com>
2025-02-01 10:56:53 -0800

    updated configuration formats and structures, got train.py set up to pull them in correctly

--- src/train.py
+++ src/train.py
@@ -6,43 +6,34 @@ import os
 import torch
 import glob
 import shutil
-import multiprocessing
 import psutil
 import timeit
-from torch.utils.tensorboard import SummaryWriter
 
 @click.command()
 @click.option('--model_name', default=None, help='Path to the model configuration file.')
 @click.option('--data_name', default=None, help='Path to the data configuration file.')
 def train_detector(model_name, data_name):
+    print(f"Loading configuration files...")
     config = 'cfg/settings.yaml'
     phase = 'train'
-    print(f"Loading configuration files...")
 
     with open(config, 'r') as file:
         settings = yaml.safe_load(file)
 
-    if model_name is None:
-        model_config = settings['model_config_default']
-    else:
-        model_config = os.path.join(settings['model_config_dir'], model_name) + '.yaml'
-
-    if data_name is None:
-        data_config = settings['data_config_default']
-    else:
-        data_config = os.path.join(settings['data_config_dir'], data_name) + '.yaml'
-
+    model_name = model_name or settings['model_config_default']
+    model_config = os.path.join(settings['model_config_dir'], model_name) + '.yaml'
     with open(model_config, 'r') as file:
         model_settings = yaml.safe_load(file)
         print(f"Found model configuration file {model_config} and successfully loaded")
 
+    data_name = data_name or settings['data_config_default']
+    data_config = os.path.join(settings['data_config_dir'], data_name) + '.yaml'
     with open(data_config, 'r') as file:
         data_settings = yaml.safe_load(file)
         print(f"Found data configuration file {data_config} and successfully loaded")
 
-    pretrained_weights_file = model_settings['train']['pretrained_model']
-    epochs = settings['epochs']
-    project = settings[phase]['project']
+    pretrained_file = model_settings['pretrained_model']
+    project = os.path.join(settings['runs_dir'], phase)
 
     run_number = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
     name = f'{model_name}/{data_name}/{run_number}'
@@ -62,15 +53,16 @@ def train_detector(model_name, data_name):
     else:
         print("CUDA is not available.")
 
-    print(f"Loading pretrained weights from {pretrained_weights_file}")
-    model = YOLO(pretrained_weights_file)
+    print(f"Loading pretrained weights from {pretrained_file}")
+    model = YOLO(pretrained_file)
 
     now = timeit.default_timer()
-    print(f"Training model {model_name} on data {data_name} for {epochs} epochs")
-    results = model.train(data=data_config, epochs=epochs, project=project, name=name, workers=num_workers)
+    print(f"Training model {model_name} on data {data_name}")
+    results = model.train(data=data_config, project=project, name=name, workers=num_workers,
+                            cfg=model_settings['hyp'])
     print(f"Training completed in {timeit.default_timer() - now} seconds using {num_workers} workers")
 
-    # Check for .pt files and move them
+    # Check for downloaded .pt files and move them out of the current directory
     amp_check_models_dir = settings['amp_check_models_dir']
     for pt_file in glob.glob("*.pt"):
         if not os.path.islink(pt_file):
