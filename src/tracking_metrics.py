3f45a5bbb5d6ce877a414d02bc42b12309e4a9e3
Patrick French <psfrench76@gmail.com>
2025-02-24 19:51:35 -0800

    incomplete commit to catch up changes in tracking_metrics before moving

--- src/tracking_metrics.py
+++ src/tracking_metrics.py
@@ -1,38 +1,8 @@
-import numpy as np
 import motmetrics as mm
+from .settings import settings
 
 
 class TrackingMetrics:
-    METRIC_NAMES = {
-        'hota_alpha': 'HOTA',
-        'assa_alpha': 'ASSA',
-        'deta_alpha': 'DETA',
-        'num_frames': 'Frames',
-        'idf1': 'ID_F1',
-        'idp': 'ID_Precision',
-        'idr': 'ID_Recall',
-        'recall': 'Recall',
-        'precision': 'Precision',
-        'num_objects': 'Objects',
-        'mostly_tracked': 'Mostly_Track',
-        'partially_tracked': 'Part_Track',
-        'mostly_lost': 'Mostly_Lost',
-        'num_false_positives': 'False_Pos',
-        'num_misses': 'Misses',
-        'num_switches': 'Switches',
-        'num_fragmentations': 'Fragmt.',
-        'mota': 'MOTA',
-        'motp': 'MOTP',
-        'id_global_assignment': 'ID_Global_Assn',
-        'obj_frequencies': 'Object_Freq',
-        'num_unique_objects': 'Num_Unique_Objects',
-        'idfp': 'ID_False_Pos',
-        'idfn': 'ID_False_Neg',
-        'idtp': 'ID_True_Pos',
-        'num_matches': 'Num_Matches'
-    }
-    HOTA_METRICS = ['hota_alpha', 'assa_alpha', 'deta_alpha']
-
     def __init__(self):
         self.acc = [mm.MOTAccumulator(auto_id=False)]
         self.acc_names = ['Default']
@@ -42,32 +12,11 @@ class TrackingMetrics:
     def update(self, gt_ids, tr_ids, distances, frame_id):
         self.acc[0].update(gt_ids, tr_ids, distances, frameid=frame_id)
 
-    # modified from example here: https://github.com/cheind/py-motmetrics
-    def compute_hota(self, df_gt, df_pred):
-        # Require different thresholds for matching
-        th_list = np.arange(0.05, 0.99, 0.05)
-        res_list = mm.utils.compare_to_groundtruth_reweighting(df_gt, df_pred, "iou", distth=th_list)
-        return res_list
-
-    def compute(self, metrics, outfile=None, df_gt=None, df_pred=None, printsum=False):
+    def compute(self, metrics, outfile=None, printsum=False):
         self.metrics = metrics
 
-        if any([m in self.metrics for m in self.HOTA_METRICS]):
-            if df_gt and df_pred:
-
-                hota_acc = self.compute_hota(df_gt, df_pred)
-                self.acc.extend(hota_acc)
-                #self.acc = hota_acc
-                self.acc_names.append("HOTA")
-
-            else:
-                raise "GT or Pred results not provided in call to 'TrackingMetrics.compute()'"
-        #print(self.acc)
-        #print(self.metrics)
-        #print(self.acc_names)
-
         summary = self.mh.compute_many(self.acc, metrics=self.metrics, names=self.acc_names)
-        #summary = self.mh.compute_many(self.acc, self.metrics, generate_overall=True)
+
         if outfile:
             summary.to_csv(outfile)
 
@@ -81,8 +30,7 @@ class TrackingMetrics:
         strsummary = mm.io.render_summary(
             summary.iloc[[-1], :],  # Use list to preserve `DataFrame` type
             formatters=self.mh.formatters,
-            namemap=self.METRIC_NAMES,
-            #namemap={"hota_alpha": "HOTA", "assa_alpha": "ASSA", "deta_alpha": "DETA"},
+            namemap=settings['tracking_metrics_names']
         )
         print("\n" + strsummary)
 
