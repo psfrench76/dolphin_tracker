a928c294ae7aa90d56348782c8ea1cb7ae53d902
Patrick French <psfrench76@gmail.com>
2025-03-05 13:00:10 -0800

    modularized, settingsized video generation

--- src/utils/generate_annotated_video.py
+++ src/utils/generate_annotated_video.py
@@ -1,7 +1,7 @@
 """
 This script takes the original .jpg frames of a video and the output from dolphin_tracker (.txt file in MOT15 format),
 and generates an .mp4 video. The video is saved to the output location specified in --output_folder, and is resized
-according to the --resize_ratio option (this doesn't significantly affect processing speed, and is primarily present
+according to the --resize option (this doesn't significantly affect processing speed, and is primarily present
 to improve file transfer time).
 """
 
@@ -14,11 +14,11 @@ def main():
     parser.add_argument('--image_folder', required=True, type=Path, help="Path to the folder containing image frames.")
     parser.add_argument('--bbox_file', required=True, type=Path, help="Path to the bounding box prediction file (MOT15 format).")
     parser.add_argument('--output_folder', required=True, type=Path, help="Path to the output folder for the video file.")
-    parser.add_argument('--resize_ratio', default=1.0, type=float, help="Ratio by which to resize the frames (e.g., 0.5 for half size).")
+    parser.add_argument('--resize', default=1.0, type=float, help="If less than 10, ratio by which to resize the frames (e.g., 0.5 for half size). If greater than 10, width of the output video.")
     parser.add_argument('--gt', action='store_true', help="Label as ground truth video")
     args = parser.parse_args()
 
-    generate_video(args.image_folder, args.bbox_file, args.output_folder, args.resize_ratio, args.gt)
+    generate_video(args.image_folder, args.bbox_file, args.output_folder, args.resize, args.gt)
 
 if __name__ == '__main__':
     main()
--- src/utils/inc/video_generation.py
+++ src/utils/inc/video_generation.py
@@ -1,14 +1,14 @@
 """
 This script takes the original .jpg frames of a video and the output from dolphin_tracker (.txt file in MOT15 format),
 and generates an .mp4 video. The video is saved to the output location specified in --output_folder, and is resized
-according to the --resize_ratio option (this doesn't significantly affect processing speed, and is primarily present
+according to the --resize option (this doesn't significantly affect processing speed, and is primarily present
 to improve file transfer time).
 """
 
 import cv2
 import re
 import pandas as pd
-from tqdm import tqdm
+from tqdm import tqdm # This is for the progress bar
 import numpy as np
 import yaml
 from pathlib import Path
@@ -18,26 +18,14 @@ from .settings import settings
 # image_folder: (Path) Path to the folder containing image frames.
 # bbox_file: (Path) Path to the bounding box prediction file (MOT15 format).
 # output_folder: (Path) Path to the output folder for the video file.
-# resize_ratio: (float) Ratio by which to resize the frames (e.g., 0.5 for half size).
+# resize: (float) If less than 10, ratio by which to resize the frames (e.g., 0.5 for half size). If greater than 10, width of the output video.
 # gt: (bool) Label as ground truth video
 
-def generate_video(image_folder, bbox_file, output_folder, resize_ratio, gt):
-    # Read the bounding box file
-    bboxes = pd.read_csv(bbox_file, header=None)
-    bboxes.columns = settings['bbox_file_columns']
-
-    # Sort images by filename to ensure correct order
+def generate_video(image_folder, bbox_file, output_folder, resize, gt):
     image_files = sorted([f for f in image_folder.iterdir() if f.suffix in settings['image_file_extensions']])
     if not image_files:
         print("No images found in the specified folder.")
         return
-
-    run_name = output_folder.name
-
-    if gt:
-        output_video_path = output_folder / f"{run_name}_{settings['gt_video_suffix']}"
-    else:
-        output_video_path = output_folder / f"{run_name}_{settings['prediction_video_suffix']}"
 
     # Initialize video writer
     first_image_path = image_files[0]
@@ -45,20 +33,34 @@ def generate_video(image_folder, bbox_file, output_folder, resize_ratio, gt):
     height, width, _ = first_image.shape
 
     # Calculate new dimensions based on the resize ratio
-    new_width = int(width * resize_ratio)
-    new_height = int(height * resize_ratio)
+    if resize > 10:
+        new_width = int(resize)
+        new_height = int(height * new_width / width)
+    else:
+        new_width = int(width * resize)
+        new_height = int(height * resize)
+
+    run_name = output_folder.name
+    if gt:
+        output_video_path = output_folder / f"{run_name}_{settings['gt_video_suffix']}"
+    else:
+        output_video_path = output_folder / f"{run_name}_{settings['prediction_video_suffix']}"
 
     # Set the codec and create VideoWriter object
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     video_writer = cv2.VideoWriter(str(output_video_path), fourcc, 30, (new_width, new_height))
 
-    # Assign colors to track IDs, excluding cyan
-    colors = [
-        (255, 0, 0), (0, 255, 0), (255, 255, 0),
-        (255, 0, 255), (128, 0, 128), (128, 128, 0),
-        (128, 0, 0), (0, 128, 0), (0, 128, 128)
-    ]
+    # Get track colors and build tuples for cv2
+    colors = [(r, g, b) for r, g, b in settings['track_colors']]
     track_colors = {}
+
+    # Text settings
+    font_scale = 0.4
+    font_height = int(22 * font_scale)  # Just based on default cv2 font height
+    text_vertical_margin = 5
+
+    # Get bounding boxes from the CSV file
+    all_bboxes = _get_bboxes_from_csv(bbox_file)
 
     # Iterate over each frame
     for image_file in tqdm(image_files, desc="Processing frames"):
@@ -69,40 +71,39 @@ def generate_video(image_folder, bbox_file, output_folder, resize_ratio, gt):
             print(f"Could not parse frame number from file: {image_file}")
             continue
 
+        # Get frame and resize
         frame = cv2.imread(str(image_file))
-
-        # Resize the frame
         frame = cv2.resize(frame, (new_width, new_height))
 
-        # Filter bounding boxes for the current frame and resize their coordinates
-        frame_bboxes = bboxes[bboxes['frame'] == frame_number]
+        # Filter bounding boxes for the current frame
+        frame_bboxes = all_bboxes[all_bboxes['frame'] == frame_number]
 
         # Draw bounding boxes and labels
         for _, row in frame_bboxes.iterrows():
             if np.isnan(row['id']):
                 continue
 
-            center_x = int(row['x'] * new_width)
-            center_y = int(row['y'] * new_height)
-            w = int(row['w'] * new_width)
-            h = int(row['h'] * new_height)
-
-            x = int(center_x - w / 2)
-            y = int(center_y - h / 2)
-
-            track_id = row['id']
+            # Get bounding box information from row
+            bbox = _get_bbox_from_yolo_coordinates(row['x'], row['y'], row['w'], row['h'], new_width, new_height)
+            track_id = int(row['id'])
 
             # Assign a color to each track ID
             if track_id not in track_colors:
                 track_colors[track_id] = colors[len(track_colors) % len(colors)]
 
-            # Draw rectangle and label
-            cv2.rectangle(frame, (x, y), (x + w, y + h), track_colors[track_id], 2)
-            cv2.putText(frame, f'ID: {track_id}', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, track_colors[track_id], 2)
+            # Get track label location
+            track_label_x = bbox['x_top_left']
+            track_label_y = bbox['y_top_left'] - text_vertical_margin
+            if track_label_y - font_height - text_vertical_margin < 0:
+                track_label_y = bbox['y_top_left'] + bbox['h'] + text_vertical_margin + font_height
+
+            # Draw bounding box, center point, and track ID
+            cv2.rectangle(frame, (bbox['x_top_left'], bbox['y_top_left']), (bbox['x_bottom_right'], bbox['y_bottom_right']), track_colors[track_id], 1, cv2.LINE_AA)
+            cv2.circle(frame, (bbox['center_x'], bbox['center_y']), 1, track_colors[track_id], -1)
+            cv2.putText(frame, f'ID: {track_id}', (track_label_x, track_label_y), cv2.FONT_HERSHEY_DUPLEX, font_scale, track_colors[track_id], 1, cv2.LINE_AA)
 
         # Add frame ID to the lower left corner
-        cv2.putText(frame, f'Frame: {image_file.name}', (10, new_height - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                    (255, 255, 255), 2)
+        cv2.putText(frame, f'Frame: {image_file.name}', (10, new_height - 10), cv2.FONT_HERSHEY_DUPLEX, font_scale, (255, 255, 255), 1, cv2.LINE_AA)
 
         # Write the frame to the video
         video_writer.write(frame)
@@ -110,3 +111,21 @@ def generate_video(image_folder, bbox_file, output_folder, resize_ratio, gt):
     # Release the video writer
     video_writer.release()
     print(f"Video saved to {output_video_path}")
+
+def _get_bboxes_from_csv(csv_file):
+    bboxes = pd.read_csv(csv_file, header=None)
+    bboxes.columns = settings['bbox_file_columns']
+    return bboxes
+
+def _get_bbox_from_yolo_coordinates(x, y, w, h, img_width, img_height):
+    bbox = {
+        'center_x': x * img_width,
+        'center_y': y * img_height,
+        'w': w * img_width,
+        'h': h * img_height,
+        'x_top_left': (x - w / 2) * img_width,
+        'y_top_left': (y - h / 2) * img_height,
+        'x_bottom_right': (x + w / 2) * img_width,
+        'y_bottom_right': (y + h / 2) * img_height
+    }
+    return {k: int(v) for k, v in bbox.items()}
