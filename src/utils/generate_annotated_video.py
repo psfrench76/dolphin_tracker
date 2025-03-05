5faf69c0894840dcfc33db553f75faed78569f12
Patrick French <psfrench76@gmail.com>
2025-03-05 11:01:11 -0800

    separated out main function from generate_video

---
+++ src/utils/generate_annotated_video.py
@@ -0,0 +1,24 @@
+"""
+This script takes the original .jpg frames of a video and the output from dolphin_tracker (.txt file in MOT15 format),
+and generates an .mp4 video. The video is saved to the output location specified in --output_folder, and is resized
+according to the --resize_ratio option (this doesn't significantly affect processing speed, and is primarily present
+to improve file transfer time).
+"""
+
+import argparse
+from pathlib import Path
+from inc.generate_annotated_video import generate_video
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate a video from image frames and bounding box predictions.")
+    parser.add_argument('--image_folder', required=True, type=Path, help="Path to the folder containing image frames.")
+    parser.add_argument('--bbox_file', required=True, type=Path, help="Path to the bounding box prediction file (MOT15 format).")
+    parser.add_argument('--output_folder', required=True, type=Path, help="Path to the output folder for the video file.")
+    parser.add_argument('--resize_ratio', default=1.0, type=float, help="Ratio by which to resize the frames (e.g., 0.5 for half size).")
+    parser.add_argument('--gt', action='store_true', help="Label as ground truth video")
+    args = parser.parse_args()
+
+    generate_video(args.image_folder, args.bbox_file, args.output_folder, args.resize_ratio, args.gt)
+
+if __name__ == '__main__':
+    main()
