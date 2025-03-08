834dc63c39907f3f6a41367d74a9a0e990d06151
Patrick French <psfrench76@gmail.com>
2025-03-08 12:24:38 -0800

    refactored parse_video script to use argparse/path

--- src/utils/parse_video.py
+++ src/utils/parse_video.py
@@ -1,16 +1,15 @@
-import subprocess
-import click
-import os
-from PIL import Image
 from inc.video_processing import extract_frames
+import argparse
+from pathlib import Path
 
 
-@click.command()
-@click.argument('input_video', type=click.Path(exists=True))
-@click.argument('output_folder', type=click.Path())
-def main(input_video, output_folder):
-    extract_frames(input_video, output_folder)
-
+def main():
+    parser = argparse.ArgumentParser(
+        description="Extract frames from a video.")
+    parser.add_argument('input_video', type=Path, help="Path to the video file.")
+    parser.add_argument('output_folder', type=Path, help="Path to the output folder for the frames.")
+    args = parser.parse_args()
+    extract_frames(args.input_video, args.output_folder)
 
 if __name__ == '__main__':
     main()
