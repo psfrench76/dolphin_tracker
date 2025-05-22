343f37d602fbb0ee6be95688c0956893306d78bd
Patrick French <psfrench76@gmail.com>
2025-05-22 09:27:03 -0700

    added angle filtering to batch eval orientations

--- src/submit_eval_orientations.py
+++ src/submit_eval_orientations.py
@@ -16,7 +16,8 @@ def validate_args(dataset, output_folder, weights, augment):
 @click.option('--output_folder', '-o', required=True, help='Path to the output folder.')
 @click.option('--weights', '-w', default='default', help='PPath to the model weights file.')
 @click.option('--augment', '-a', is_flag=True, help='Use data augmentation.')
-def main(dataset, output_folder, weights, augment):
+@click.option('--filter_angles', '-fa', is_flag=True, help='Evaluate using angle filtering and averaging. Not compatible with augment.')
+def main(dataset, output_folder, weights, augment, filter_angles):
     validate_args(dataset, output_folder, weights, augment)
 
     # Set environment variables
@@ -24,6 +25,7 @@ def main(dataset, output_folder, weights, augment):
     os.environ['OUTPUT_FOLDER'] = output_folder
     os.environ['WEIGHTS'] = weights
     os.environ['AUGMENT'] = '--augment' if augment else ''
+    os.environ['FILTER_ANGLES'] = '--filter_angles' if filter_angles else ''
 
     subprocess.run(["sbatch", "utils/hpc/orient_eval_job.sbatch"])
     print("Job submitted.")
